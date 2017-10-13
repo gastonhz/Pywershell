83b7b1cad46f5ef50422f51f10921464935a2284
Gastón Galarza <gaston.galarza.n7@gmail.com>
2017-10-13 19:59:07 -0300

    Update Temporales.py

--- Temporales.py
+++ Temporales.py
@@ -1,3 +1,5 @@
+#Este script borra archivos temporales en algunas rutas del sistema.
+#Gastón Galarza, MDA-Tecnico 09/2017, github.com/gastonhz/Scripts
 import os
 import re
 hostname=input("Ingresar IP: ")
